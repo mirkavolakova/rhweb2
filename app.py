89f69760a3a57dfc8575db387bd3ec36a8c9a66b
Sanqui <gsanky@gmail.com>
2015-02-17 11:01:39 +0100

    Initial commit.  Nothing really works.

---
+++ app.py
@@ -0,0 +1,139 @@
+# coding: utf-8
+from __future__ import absolute_import, unicode_literals, print_function
+
+import db
+from sqlalchemy import or_, and_, asc, desc, func
+from datetime import datetime
+from functools import wraps # We need this to make Flask understand decorated routes.
+import hashlib
+
+from werkzeug import secure_filename
+from flask import Flask, render_template, request, flash, redirect, session, abort, url_for, make_response, g
+from wtforms import Form, BooleanField, TextField, TextAreaField, PasswordField, RadioField, SelectField, SelectMultipleField, BooleanField, HiddenField, SubmitField, validators, ValidationError, widgets
+
+app = Flask('rhforum')
+app.config.from_pyfile('config.py')
+
+
+class PostForm(Form):
+    text = TextAreaField('Text', [validators.required()])
+    submit = SubmitField('Odeslat')
+
+class ThreadForm(PostForm):
+    name = TextField('Nadpis', [validators.required()])
+
+
+@app.template_filter('datetime')
+def datetime_format(value, format='%d. %m. %Y %H:%M:%S'):
+    if not value: return "-"
+    if isinstance(value, unicode): return value
+    return value.strftime(format)
+
+@app.before_request
+def before_request():
+    if 'user_id' in session:
+        g.user = db.session.query(db.User).get(session['user_id'])
+        if not g.user:
+            # TODO
+            pass
+    else:
+        g.user = None
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db.session.close()
+    db.session.remove()
+
+@app.route("/")
+def index():
+    fora = db.session.query(db.Forum)
+    return render_template("index.html", fora=fora)
+
+class LoginForm(Form):
+    name = TextField('Jméno', [validators.required()])
+    submit = SubmitField('Přihlásit se')
+
+@app.route("/login", methods="GET POST".split())
+def login():
+    login_form = LoginForm(request.form)
+    failed = False
+    if request.method == 'POST' and login_form.validate():
+        g.user = db.session.query(db.User).filter(db.User.name == login_form.name.data).scalar()
+        if not g.user: failed = True
+        else:
+            session['user_id'] = g.user.id
+            session.permanent = True
+            flash("Jste přihlášeni.")
+            return redirect("/")
+    
+    return render_template("login.html", login_form=login_form, failed=failed)
+
+class RegisterForm(Form):
+    name = TextField('Jméno', [validators.required()])
+    email = TextField('Email', [validators.required()])
+    submit = SubmitField('Zaregistrovat')
+
+@app.route("/register", methods="GET POST".split())
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = db.User(name=form.name.data, email=form.email.data, timestamp=datetime.now())
+        db.session.add(user)
+        db.session.commit()
+        g.user = user
+        session['user_id'] = g.user.id
+        session.permanent = True
+        
+        flash("Registrace proběhla úspěšně.")
+        return redirect("/")
+    
+    return render_template("register.html", form=form)
+
+@app.route("/logout")
+def logout():
+    if 'user_id' in session:
+        session.pop('user_id')
+        flash("Odhlášení proběhlo úspěšně.")
+        return redirect("/")
+
+@app.route("/<int:forum_id>", methods="GET POST".split())
+@app.route("/<int:forum_id>-<forum_identifier>", methods="GET POST".split())
+def forum(forum_id, forum_identifier=None):
+    forum = db.session.query(db.Forum).get(forum_id)
+    form = ThreadForm(request.form)
+    if request.method == 'POST' and form.validate():
+        thread = db.Thread(forum=forum, author=g.user, timestamp=datetime.now(),
+            name=form.name.data)
+        post = db.Post(thread=thread, author=g.user, timestamp=datetime.now(),
+            text=form.text.data)
+        pass
+        # TODO
+    return render_template("forum.html", forum=forum, form=form)
+
+
+@app.route("/<int:forum_id>/<int:topic_id>", methods="GET POST".split())
+@app.route("/<int:forum_id>-<forum_identifier>/<int:thread_id>-<thread_identifier>", methods="GET POST".split())
+def thread(forum_id, thread_id, forum_identifier=None, thread_identifier=None):
+    thread = db.session.query(db.Thread).get(thread_id)
+    form = PostForm(request.form)
+    if request.method == 'POST' and form.validate():
+        post = db.Post(thread=thread, author=g.user, timestamp=datetime.now(),
+            text=form.text.data)
+        db.session.commit()
+        return redirect(thread.url+"#latest") # TODO id
+    
+    return render_template("thread.html", thread=thread, forum=thread.forum, form=form, now=datetime.now())
+
+if __name__ == "__main__":
+    app.run(host="", port=8080, debug=True, threaded=True)
+
+
+
+
+
+
+
+
+
+
+
---
+++ db.py
@@ -0,0 +1,122 @@
+# encoding: utf-8
+from __future__ import absolute_import, unicode_literals, print_function
+from datetime import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
+from sqlalchemy.schema import Column, ForeignKey, Table
+from sqlalchemy.types import DateTime, Integer, Unicode, Enum, UnicodeText, Boolean, TypeDecorator
+
+engine = create_engine(open("db").read(), encoding=b"utf8")#, pool_size = 100, pool_recycle=4200) # XXX
+# pool_recycle is to prevent "server has gone away"
+session = scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+Base = declarative_base(bind=engine)
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(Unicode(256))
+    email = Column(Unicode(256))
+    homepage = Column(Unicode(256))
+    avatar_url = Column(Unicode(256))
+    rights = Column(Integer)
+    timestamp = Column(DateTime)
+    laststamp = Column(DateTime)
+    rights = Column(Integer)
+    profile = Column(UnicodeText)
+    
+    @property
+    def num_posts(self):
+        return session.query(Post).filter(Post.author == self).count()
+
+class Forum(Base):
+    __tablename__ = 'fora'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    identifier = Column(Unicode(256))
+    name = Column(Unicode(256))
+    description = Column(UnicodeText)
+    minviewrights = Column(Integer)
+    minpostrights = Column(Integer)
+    
+    @property
+    def url(self):
+        return "/{}-{}".format(self.id, self.identifier)
+        
+    @property
+    def last_post(self):
+        return session.query(Post).join(Post.thread).filter(Thread.forum == self).order_by(Post.timestamp).first()
+    
+
+class Thread(Base):
+    __tablename__ = 'threads'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(Unicode(256))
+    description = Column(UnicodeText)
+    forum_id = Column(Integer, ForeignKey('fora.id'), nullable=False)
+    forum = relationship("Forum", backref='threads', order_by="Thread.laststamp")
+    author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    author = relationship("User", backref='threads')
+    timestamp = Column(DateTime)
+    laststamp = Column(DateTime)
+    pinned = Column(Boolean)
+    
+    @property
+    def url(self):
+        return "/{}-{}/{}-{}".format(self.forum.id, self.forum.identifier, self.id, self.name)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(Unicode(256))
+    thread_id = Column(Integer, ForeignKey('threads.id'), nullable=False)
+    thread = relationship("Thread", backref='posts', order_by="Post.timestamp")
+    author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    author = relationship("User", backref='posts')
+    timestamp = Column(DateTime)
+    text = Column(UnicodeText)
+
+class ThreadRead(Base):
+    __tablename__ = 'threads_read'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    thread_id = Column(Integer, ForeignKey('threads.id'), nullable=False)
+    thread = relationship("Thread")
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user = relationship("User", backref='threads_read')
+    last_post = Column(Integer)
+
+#if __name__ == "__main__":
+#if raw_input('Drop all? ') == 'y':
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+f = Forum(name="Novinky", identifier="novinky", description="Novinky ve světě RH")
+session.add(f)
+f2 = Forum(name="Obecné", identifier="obecne", description="Posty o čemkoli")
+session.add(f2)
+f3 = Forum(name="Ostatní", identifier="ostatni", description="Popisek")
+session.add(f3)
+
+u = User(name="Uživatel")
+session.add(u)
+
+t = Thread(name="První téma na fóru", description="Yay!", forum=f, author=u)
+session.add(t)
+
+p = Post(thread=t, author=u, text="First post!  <b>Test</b>!", timestamp=datetime.now())
+session.add(t)
+p = Post(thread=t, author=u, text="Second post made to test the layout. \nLorizzle ipsizzle cool da bomb amizzle, dawg adipiscing dawg. Dope sapizzle gangsta, mammasay mammasa mamma oo sa volutpizzle, suscipizzle izzle, gravida vel, arcu. Pellentesque eget tortor. Sizzle erizzle. Brizzle at dolor dapibus turpis tempizzle gangsta. Mauris fizzle nibh things turpizzle. Vestibulum in tortor. Pellentesque doggy rhoncizzle nisi. In hac stuff its fo rizzle dictumst. Its fo rizzle dapibizzle. Curabitizzle tellus gangster, pretium shizzlin dizzle, yippiyo mofo, eleifend vitae, nunc. Shizzlin dizzle suscipizzle. Integizzle sempizzle fo shizzle my nizzle sizzle doggy. Donizzle shizznit doggy mauris. Phasellizzle shiz elit izzle yo mammasay mammasa mamma oo sa tincidunt. Crunk a owned. Vestibulizzle cool check out this sed maurizzle elementum tristique. Nunc izzle its fo rizzle sit fo shizzle erizzle ultricizzle check out this. Bizzle crazy i'm in the shizzle, rizzle izzle, i saw beyonces tizzles and my pizzle went crizzle quis, adipiscing the bizzle, dui. Hizzle velit shut the shizzle up, aliquizzle consequat, pharetra non, dictizzle sizzle, turpizzle. Shiz mah nizzle. Izzle lorem. Brizzle vitae pizzle izzle libero commodo adipiscing. Fusce fo shizzle mah nizzle fo rizzle, mah home g-dizzle augue its fo rizzle gizzle dizzle phat. \nCrazy fermentum shizzle my nizzle crocodizzle non dawg. Suspendisse lorizzle dope, sollicitudin bling bling, you son of a bizzle izzle, that's the shizzle nizzle, justo. Donizzle faucibus porttitizzle boofron. Nunc feugiat, tellizzle shizzlin dizzle ornare fo shizzle mah nizzle fo rizzle, mah home g-dizzle, sapien ass tincidunt owned, egizzle dapibus pede enim izzle lorem. Phasellizzle doggy leo, bling bling izzle, tempus izzle, dope izzle, sapizzle. Things ma nizzle check out this vizzle ipsum. Sizzle ante mammasay mammasa mamma oo sa, suscipizzle vitae, vestibulizzle et, rutrizzle eu, velizzle. Maurizzle black mauris. Sizzle magna sizzle amizzle risus iaculizzle dizzle.", timestamp=datetime.now())
+session.add(t)
+
+session.commit()
+
+
+
+
