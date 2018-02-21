dd268c4afa243689711233528ca9f29ec2c1dab1
Sanqui <gsanky@gmail.com>
2018-02-21 19:21:05 +0200

    Vyjezdy

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template, request, flash, redirect, session, abo
 
 app = Flask('rhweb2')
 
+@app.context_processor
+def new_template_globals():
+    return {
+        'zip': zip,
+    }
+
 @app.route("/")
 def o_nas():
     return render_template("o-nas.html")
@@ -10,9 +16,14 @@ def o_nas():
 @app.route("/kontakt")
 def kontakt():
     return render_template("kontakt.html")
+    
 @app.route("/komunita")
 def komunita():
     return render_template("komunita.html")
+
+@app.route("/vyjezdy")
+def vyjezdy():
+    return render_template("vyjezdy.html")
 
 if __name__ == "__main__":
     app.run(host="", port=9011, debug=True)
