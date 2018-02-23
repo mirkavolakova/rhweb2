a8655c91d5da6910b9b21aea7ed7a6d4f6febacb
Sanqui <gsanky@gmail.com>
2018-02-23 21:06:05 +0200

    Add vague links everywhere

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ app = Flask('rhweb2')
 
 @app.context_processor
 def new_template_globals():
+    urls = {
+        "facebook": "https://facebook.com/retroherna",
+        "youtube": "https://www.youtube.com/channel/UCJNNkhuJNO5dujOhy9r-jdA",
+        "twitter": "https://twitter.com/hernihistorie",
+        "discord": "https://discord.gg/9jajeqZ",
+        "bankaccount": "https://www.moneta.cz/firmy/instituce-a-verejna-sprava/transparentni-ucty/001/-/transparent-account/8686868686",
+        "forum": "/forum/",
+        "email": "mailto:info@retroherna.org"
+    }
     return {
         'zip': zip,
+        'urls': urls,
     }
 
 @app.route("/")
@@ -24,6 +34,10 @@ def komunita():
 @app.route("/vyjezdy")
 def vyjezdy():
     return render_template("vyjezdy.html")
+
+@app.route("/clanky")
+def clanky():
+    return render_template("clanky.html")
 
 if __name__ == "__main__":
     app.run(host="", port=9011, debug=True)
